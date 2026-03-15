9547605c91174a60e74670bbd5f497ce1d66d379
nanoluna1 <92677168+nanoluna1@users.noreply.github.com>
2026-03-14 20:57:31 -0500

    just added the simple stuff

---
+++ HOWARD.cs
@@ -0,0 +1,16 @@
+using System;
+using HarmonyLib;
+using MelonLoader;
+using Il2CppRUMBLE.Players.Subsystems;
+using System.Reflection;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(HowardIssue.HOWARD), "HowardSkillIssue", "1.0.0", "Nano")]
+[assembly: MelonColor(127, 52, 235, 131)]
+[assembly: MelonGame("Buckethead Entertainment", "RUMBLE")]
+
+namespace HowardIssue
+{
+
+
+}
